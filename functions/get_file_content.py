60cd519db2ab53192bd5621a4a97395b4d3d6f95
Brime-c <olabarriaga.felipe@gmail.com>
2025-06-19 22:22:02 -0300

    added get_file_content

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,1 +1,23 @@
+import os
+
 def get_file_content(working_directory, file_path):
+    # Safeguard against None or empty file_path
+    if not os.path.abspath(os.path.join(working_directory, file_path)).startswith(os.path.abspath(working_directory)):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    elif not os.path.isfile(os.path.join(working_directory, file_path)):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+    max_characters = 10000
+    try:
+        with open(os.path.join(working_directory, file_path), 'r') as f:
+            file_content_string = f.read()    
+    
+    except Exception as e:
+        return f'Error:{str(e)}'   
+
+    if len(file_content_string) > max_characters:
+        truncated_message = f"\n\n[... File {file_path} truncated to {max_characters} characters...]"
+        file_content_string = file_content_string[0:10000] + truncated_message
+    return file_content_string
+        
+    
--- tests.py
+++ tests.py
@@ -1,6 +1,5 @@
-from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-print(get_files_info("calculator", "."))  # Should list files in the current directory
-print(get_files_info("calculator", "pkg"))  # Should list files in the pkg directory
-print(get_files_info("calculator", "/bin"))  # Should return an error since /bin is outside the working directory
-print(get_files_info("calculator", "../"))  # Should return an error since ../ is outside the working directory
+print(get_file_content("calculator", "main.py")) 
+print(get_file_content("calculator", "pkg/calculator.py")) 
+print(get_file_content("calculator", "/bin/cat"))  # This should return an error since it's outside the working directory
